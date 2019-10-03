bd13c4b351bd2bcf624cd0b1f4dc46251ecfcbe6
ncallan <55205605+ncallan@users.noreply.github.com>
2019-10-03 17:06:32 -0400

    Update routes.py

--- app/routes.py
+++ app/routes.py
@@ -5,4 +5,4 @@ from records import Database
 @app.route('/')
 @app.route('/<name>')
 def index(name="boi"):
-    return "<html><body bgcolor=\"#202\" color=\"#fff\"><center>Hello, " + name + ".</center></body></html>"
+    return "<html><body bgcolor=\"#202\" color=\"#fff\"><center><p>Hello, " + name + ".</p></center></body></html>"
