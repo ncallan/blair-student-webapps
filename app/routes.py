b6b4497e63d78f9fb43c7e00d30b0ecf76e64955
ncallan <55205605+ncallan@users.noreply.github.com>
2019-10-03 13:52:41 -0400

    Update routes.py

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,8 @@
 from app import app
 import os
+from records import Database
 
 @app.route('/')
 @app.route('/<name>')
 def index(name="boi"):
-    return os.environ["DATABASE_URL"]
+    return Database(os.environ["DATABASE_URL"])
