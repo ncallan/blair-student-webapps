697323240a239383ed04b8b6bf2fcc79f49b85f5
ncallan <55205605+ncallan@users.noreply.github.com>
2019-10-03 20:45:21 -0400

    Add explain and vars in url

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,17 @@
 from app import app, db
 
+def ohgodnotinprod(value)
+    HTMLStart = "<html><body style=\"background-color: #202; color: #fff;\"><center><code>"
+    HTMLEnd = "</code></center></body></html>"
+    return HTMLStart + str(value) + HTMLEnd
+
+
 @app.route('/')
-@app.route('/<name>')
-def index(name="boi"):
-    return "<html><body style=\"background-color: #202; color: #fff;\"><center><code>" + str([[i.id, i.data] for i in db.query("SELECT * FROM jsonData")]) + "</code></center></body></html>"
+@app.route('/<what>/<where>')
+@app.route('/select/<what>/<where>')
+def index(what="*", where="jsonData"):
+    return ohgodnotinprod(db.query("SELECT :what FROM :where", what=what, where=where).export("html"))
+
+@app.route('/explain/<what>/<where>')
+def index(what="*", where="jsonData"):
+    return ohgodnotinprod(db.query("EXPLAIN SELECT :what FROM :where", what=what, where=where).export("html"))
