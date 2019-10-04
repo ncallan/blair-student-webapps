a0f1506c1a6873b666c429042c0bb5be9fae51ad
ncallan <55205605+ncallan@users.noreply.github.com>
2019-10-03 20:26:17 -0400

    add db support

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,9 @@
+import os
+
+from records import Database
 from flask import Flask
 
 app = Flask(__name__)
+db = Database(os.environ["DATABASE_URL"])
 
 from app import routes
--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,6 @@
-from app import app
-import os
-from records import Database
+from app import app, db
 
 @app.route('/')
 @app.route('/<name>')
 def index(name="boi"):
-    return "<html><body style=\"background-color: #202; color: #fff;\"><center><p>Hello, " + name + ".</p></center></body></html>"
+    return "<html><body style=\"background-color: #202; color: #fff;\"><center><code>" + str([[i.id, i.data] for i in db.query("SELECT * FROM jsonData")]) + "</code></center></body></html>"
