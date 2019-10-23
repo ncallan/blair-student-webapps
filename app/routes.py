b8eeb8d02c136daf903c17dfc220883d522205cc
ncallan <55205605+ncallan@users.noreply.github.com>
2019-10-22 20:31:37 -0400

    Update routes.py

--- app/routes.py
+++ app/routes.py
@@ -9,11 +9,11 @@ def ohgodnotinprod(value):
 @app.route('/')
 @app.route('/<what>/<where>')
 @app.route('/select/<what>/<where>')
-def select(what="*", where="jsonData"):
+def select(what="*", where="jsondata"):
     return ohgodnotinprod(db.query("SELECT :what FROM :where", what=what, where=where).as_dict())
 
 @app.route('/explain/<what>/<where>')
-def explain(what="*", where="jsonData"):
+def explain(what="*", where="jsondata"):
     return ohgodnotinprod(db.query("EXPLAIN SELECT :what FROM :where", what=what, where=where).as_dict())
 
 @app.route("/sms")
